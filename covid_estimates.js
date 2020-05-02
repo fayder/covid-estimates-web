d9465ef8e9e34a3ff678af8dbd11242172de54c0
Fayder Florez <fayder.florez@gmail.com>
2020-05-02 19:51:04 +0300

    adds ifr list

--- covid_estimates.js
+++ covid_estimates.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
     $('#countries').bootstrapTable();
+
+    $('#btn_medrxiv').click(function(event){
+    	if ($('#btn_medrxiv').hasClass('active')) {
+    		return;
+    	} else {
+    		$('#countries').bootstrapTable('refreshOptions', 
+    			{url: 'https://covid-estimates-backend.herokuapp.com/medrxiv'})
+    	}
+    });
+
+    $('#btn_imperial').click(function(event){
+    	if ($('#btn_imperial').hasClass('active')) {
+    		return;
+    	} else {
+    		$('#countries').bootstrapTable('refreshOptions', 
+    		{ url: 'https://covid-estimates-backend.herokuapp.com/imperial_college'})	
+    	}
+    });
+
+    $('#btn_cebm').click(function(event){
+    	if ($('#btn_cebm').hasClass('active')) {
+    		return;
+    	} else {
+    		$('#countries').bootstrapTable('refreshOptions', 
+    		{ url: 'https://covid-estimates-backend.herokuapp.com/cebm'})	
+    	}
+    });
+
+    $('#btn_iceland').click(function(event){
+    	if ($('#btn_iceland').hasClass('active')) {
+    		return;
+    	} else {
+    		$('#countries').bootstrapTable('refreshOptions', 
+    		{ url: 'https://covid-estimates-backend.herokuapp.com/iceland'})	
+    	}
+    });
 });
