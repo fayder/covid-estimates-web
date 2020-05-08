916254ff0518ed4ec61f7e4e61eee1240a074abf
Fayder Florez <fayder.florez@gmail.com>
2020-05-08 22:50:32 +0300

    adds 'select country' to charts

--- covid_charts.js
+++ covid_charts.js
@@ -38,18 +38,20 @@ $(document).ready(function() {
     var chart_max_cases;
     var chart_min_population;
     var chart_max_population;
-    var chart_cases;
-    var chart_population;
+    var chart_single_cases;
+    var chart_single_population;
+    var countries;
+    var selected_country;
 
     function initializeCharts() {
         var jqxhr = $.get('https://covid-estimates-backend.herokuapp.com/cebm?noformat=true', function(data) {
             var result = process(data);
             chart_min_cases = initalizeCasesChart('#chart_estimated_cases_min', 'Estimated min. cases', result.labels, result.min_cases);
             chart_max_cases = initalizeCasesChart('#chart_estimated_cases_max', 'Estimated max. cases', result.labels, result.max_cases);
-            chart_cases = initalizeCasesChart('#chart_estimated_cases_single', 'Estimated real cases', result.labels, result.single_cases);
+            chart_single_cases = initalizeCasesChart('#chart_estimated_cases_single', 'Estimated real cases', result.labels, result.single_cases);
             chart_min_population = initializePercentageChart('#chart_estimated_population_min', 'Estimated min. percentage of population infected', result.labels, result.min_population);
             chart_max_population = initializePercentageChart('#chart_estimated_population_max', 'Estimated max. percentage of population infected', result.labels, result.max_population);
-            chart_population = initializePercentageChart('#chart_estimated_population_single', 'Estimated percentage of population infected', result.labels, result.single_population);
+            chart_single_population = initializePercentageChart('#chart_estimated_population_single', 'Estimated percentage of population infected', result.labels, result.single_population);
         });
     }
 
@@ -87,46 +89,57 @@ $(document).ready(function() {
 
     function updateCharts(data, is_range = true) {
         var result = process(data);
-        
+
         if (is_range) {
 
+            chart_min_cases.data.labels = result.labels;
             chart_min_cases.data.datasets.forEach((dataset) => {
-            dataset.data = result.min_cases;
+                dataset.data = result.min_cases;
             });
             chart_min_cases.update();
 
+            chart_max_cases.data.labels = result.labels;
             chart_max_cases.data.datasets.forEach((dataset) => {
                 dataset.data = result.max_cases;
             });
             chart_max_cases.update();
 
+            chart_min_population.data.labels = result.labels;
             chart_min_population.data.datasets.forEach((dataset) => {
                 dataset.data = result.min_population;
             });
             chart_min_population.update();
 
+            chart_max_population.data.labels = result.labels;
             chart_max_population.data.datasets.forEach((dataset) => {
                 dataset.data = result.max_population;
             });
             chart_max_population.update();
 
         } else {
-            console.log(result.single_cases);
-            chart_cases.data.datasets.forEach((dataset) => {
+            chart_single_cases.data.labels = result.labels;
+            chart_single_cases.data.datasets.forEach((dataset) => {
                 dataset.data = result.single_cases;
             });
-            chart_cases.update();
+            chart_single_cases.update();
 
-            chart_population.data.datasets.forEach((dataset) => {
+            chart_single_population.data.labels = result.labels;
+            chart_single_population.data.datasets.forEach((dataset) => {
                 dataset.data = result.single_population;
             });
-            chart_population.update();
+            chart_single_population.update();
         }
-        
+
+        if (selected_country != null) {
+            selectCountry();
+        }
+
     }
 
     function process(data) {
-        var topCountries = data.slice(0, 6);
+        countries = data;
+        initializeDropdowns();
+        var topCountries = data.slice(0, 7);
         var labels = [];
         var min_cases = [];
         var max_cases = [];
@@ -171,7 +184,8 @@ $(document).ready(function() {
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
-                        'rgba(23, 152, 174, 0.2)'
+                        'rgba(102, 102, 255, 0.2)',
+                        'rgba(75, 153, 0, 0.2)'
 
                     ],
                     borderColor: [
@@ -180,7 +194,8 @@ $(document).ready(function() {
                         'rgba(255, 206, 86, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(1, 130, 131, 1)'
+                        'rgba(51, 51, 255, 1)',
+                        'rgba(0, 102, 0, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -225,7 +240,8 @@ $(document).ready(function() {
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
-                        'rgba(23, 152, 174, 0.2)'
+                        'rgba(23, 152, 174, 0.2)',
+                        'rgba(75, 153, 0, 0.2)'
 
                     ],
                     borderColor: [
@@ -234,7 +250,8 @@ $(document).ready(function() {
                         'rgba(255, 206, 86, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(1, 130, 131, 1)'
+                        'rgba(1, 130, 131, 1)',
+                        'rgba(0, 102, 0, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -262,6 +279,87 @@ $(document).ready(function() {
                 }
             }
         });
+    }
+
+    function initializeDropdowns() {
+        var names = [];
+        countries.forEach(function(country, index) {
+            names.push(country['name']);
+        });
+        names.sort();
+        names.forEach(function(name, index) {
+            $('#js-add-country').append(`<button class="dropdown-item js-add-country" type="button" class="btn btn-light">${name}</button>`);
+        });
+
+        $('.js-add-country').click(function(event) {
+            var country_data;
+            selected_country = this.textContent;
+            $('#dropdown-menu-btn').html(selected_country);
+            countries.forEach(function(country, index) {
+                if (country['name'] == selected_country) {
+                    addCountry(country);
+                }
+            });
+
+        });
+    }
+
+    function selectCountry() {
+        countries.forEach(function(country, index) {
+            if (country['name'] == selected_country) {
+                console.log(country);
+                addCountry(country);
+            }
+        });
+    }
+
+    function addCountry(country) {
+        chart_min_cases.data.labels.splice(0, 1);
+        chart_min_cases.data.labels.push(country['name']);
+
+        chart_single_cases.data.labels.splice(0, 1);
+        chart_single_cases.data.labels.push(country['name']);
+
+        removeFirst(chart_min_cases);
+        addCountryToChart(chart_min_cases, country['high_ifr']);
+
+        removeFirst(chart_max_cases);
+        addCountryToChart(chart_max_cases, country['low_ifr']);
+
+        removeFirst(chart_min_population);
+        addCountryToChart(chart_min_population, country['population_percentange_high']);
+
+        removeFirst(chart_max_population);
+        addCountryToChart(chart_max_population, country['population_percentange_low']);
+
+        removeFirst(chart_single_cases);
+        addCountryToChart(chart_single_cases, country['single_ifr']);
+
+        removeFirst(chart_single_population);
+        addCountryToChart(chart_single_population, country['population_percentage_single']);
+
+        chart_min_cases.update();
+        chart_max_cases.update();
+        chart_min_population.update();
+        chart_max_population.update();
+        chart_single_cases.update();
+        chart_single_population.update();
+    }
+
+    function removeFirst(chart) {
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.splice(0, 1);
+        });
+    }
+
+    function addCountryToChart(chart, data) {
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+    }
+
+    function updateChart(chart) {
+        chart.update();
     }
 
     initializeCharts();
