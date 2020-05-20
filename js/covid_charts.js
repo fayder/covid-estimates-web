c477c6ff294fcb837df751934822c1794ac65464
Fayder Florez <fayder.florez@gmail.com>
2020-05-20 20:01:05 +0300

    more emphasis on percentages on charts page

--- js/covid_charts.js
+++ js/covid_charts.js
@@ -39,11 +39,8 @@ $(document).ready(function() {
 
     // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 
-    var chart_min_cases;
-    var chart_max_cases;
     var chart_min_population;
     var chart_max_population;
-    var chart_single_cases;
     var chart_single_population;
     var countries;
     var selected_country;
@@ -51,9 +48,6 @@ $(document).ready(function() {
     function initializeCharts() {
         var jqxhr = $.get('https://covid-estimates-backend.herokuapp.com/medrxiv?noformat=true', function(data) {
             var result = process(data);
-            chart_min_cases = initalizeCasesChart('#chart_estimated_cases_min', 'Estimated min. cases', result.labels, result.min_cases);
-            chart_max_cases = initalizeCasesChart('#chart_estimated_cases_max', 'Estimated max. cases', result.labels, result.max_cases);
-            chart_single_cases = initalizeCasesChart('#chart_estimated_cases_single', 'Estimated real cases', result.labels, result.single_cases);
             chart_min_population = initializePercentageChart('#chart_estimated_population_min', 'Estimated min. percentage of population infected', result.labels, result.min_population);
             chart_max_population = initializePercentageChart('#chart_estimated_population_max', 'Estimated max. percentage of population infected', result.labels, result.max_population);
             chart_single_population = initializePercentageChart('#chart_estimated_population_single', 'Estimated percentage of population infected', result.labels, result.single_population);
@@ -65,9 +59,9 @@ $(document).ready(function() {
             }
 
             if (result.single_cases != null) {
-                $('.js-single-chart ').css('display', 'flex'); 
+                $('.js-single-chart ').css('display', 'flex');
             } else {
-                $('.js-single-chart ').css('display', 'none'); 
+                $('.js-single-chart ').css('display', 'none');
             }
 
             initializeDropdown();
@@ -115,18 +109,6 @@ $(document).ready(function() {
 
             $('.js-range-chart').css('display', 'flex');
 
-            chart_min_cases.data.labels = result.labels;
-            chart_min_cases.data.datasets.forEach((dataset) => {
-                dataset.data = result.min_cases;
-            });
-            chart_min_cases.update();
-
-            chart_max_cases.data.labels = result.labels;
-            chart_max_cases.data.datasets.forEach((dataset) => {
-                dataset.data = result.max_cases;
-            });
-            chart_max_cases.update();
-
             chart_min_population.data.labels = result.labels;
             chart_min_population.data.datasets.forEach((dataset) => {
                 dataset.data = result.min_population;
@@ -144,14 +126,8 @@ $(document).ready(function() {
         }
 
         if (result.single_cases[0] != "0.00") {
-            
-            $('.js-single-chart ').css('display', 'flex');
 
-            chart_single_cases.data.labels = result.labels;
-            chart_single_cases.data.datasets.forEach((dataset) => {
-                dataset.data = result.single_cases;
-            });
-            chart_single_cases.update();
+            $('.js-single-chart ').css('display', 'flex');
 
             chart_single_population.data.labels = result.labels;
             chart_single_population.data.datasets.forEach((dataset) => {
@@ -169,7 +145,7 @@ $(document).ready(function() {
     }
 
     function process(data) {
-        var topCountries = data.slice(0, 7);
+        var topCountries = data.slice(0, 8);
         var labels = [];
         var min_cases = [];
         var max_cases = [];
@@ -201,62 +177,6 @@ $(document).ready(function() {
         return result;
     }
 
-    function initalizeCasesChart(chart_id, chart_label, labels, data) {
-        var ctx = $(chart_id)[0].getContext('2d');
-        return new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: chart_label,
-                    data: data,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(102, 102, 255, 0.2)',
-                        'rgba(75, 153, 0, 0.2)'
-
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(51, 51, 255, 1)',
-                        'rgba(0, 102, 0, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                legend: {
-                    display: false
-                },
-                tooltips: {
-                    callbacks: {
-                        label: function(tooltipItem, data) {
-                            return numeral(tooltipItem['value']).format('0,0');
-                        }
-                    }
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            callback: function(value, index, values) {
-                                return numeral(value).format('0,0')
-                            }
-                        }
-                    }]
-                }
-            }
-        });
-    }
-
     function initializePercentageChart(chart_id, chart_label, labels, data) {
         var ctx = $(chart_id)[0].getContext('2d');
         return new Chart(ctx, {
@@ -273,7 +193,8 @@ $(document).ready(function() {
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(23, 152, 174, 0.2)',
-                        'rgba(75, 153, 0, 0.2)'
+                        'rgba(75, 153, 0, 0.2)',
+                        'rgba(153, 0, 255, 0.2)'
 
                     ],
                     borderColor: [
@@ -283,7 +204,8 @@ $(document).ready(function() {
                         'rgba(54, 162, 235, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(1, 130, 131, 1)',
-                        'rgba(0, 102, 0, 1)'
+                        'rgba(0, 102, 0, 1)',
+                        'rgba(102, 0, 204, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -342,24 +264,21 @@ $(document).ready(function() {
 
     function addCountry(country) {
         if ($('.js-single-chart').css('display') == "flex") {
-            chart_single_cases.data.labels.splice(0, 1);
-            chart_single_cases.data.labels.push(country['name']);
-            removeFirst(chart_single_cases);
-            addCountryToChart(chart_single_cases, country['single_ifr']);
+            chart_single_population.data.labels.splice(0, 1);
+            chart_single_population.data.labels.push(country['name']);
 
             removeFirst(chart_single_population);
             addCountryToChart(chart_single_population, country['population_percentage_single']);
 
-            chart_single_cases.update();
             chart_single_population.update();
-        } else {
-            chart_min_cases.data.labels.splice(0, 1);
-            chart_min_cases.data.labels.push(country['name']);
-            removeFirst(chart_min_cases);
-            addCountryToChart(chart_min_cases, country['high_ifr']);
+        }
 
-            removeFirst(chart_max_cases);
-            addCountryToChart(chart_max_cases, country['low_ifr']);
+        if ($('.js-range-chart').css('display') == "flex") {
+
+            if ($('.js-single-chart').css('display') == "none") {
+                chart_min_population.data.labels.splice(0, 1);
+                chart_min_population.data.labels.push(country['name']);
+            }
 
             removeFirst(chart_min_population);
             addCountryToChart(chart_min_population, country['population_percentange_high']);
@@ -367,8 +286,6 @@ $(document).ready(function() {
             removeFirst(chart_max_population);
             addCountryToChart(chart_max_population, country['population_percentange_low']);
 
-            chart_min_cases.update();
-            chart_max_cases.update();
             chart_min_population.update();
             chart_max_population.update();
         }
